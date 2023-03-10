d24372ad1804dbf184627433fbfa3fa0d71849c5
malko <jgotti@jgotti.org>
2023-03-10 17:59:50 +0100

    ✅ Add a bunch of tests

---
+++ depInfo_test.go
@@ -0,0 +1,66 @@
+/*
+Copyright © 2023 Jonathan Gotti <jgotti at jgotti dot org>
+SPDX-FileType: SOURCE
+SPDX-License-Identifier: MIT
+SPDX-FileCopyrightText: 2023 Jonathan Gotti <jgotti@jgotti.org>
+*/
+
+package packageJson
+
+import (
+	"path/filepath"
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestPackageJson_SatisfyWorskpaceDep(t *testing.T) {
+	overridePkgDir := func(p *PackageJSON, dirJoin string) {
+		p.Dir = filepath.Join(p.Dir, dirJoin)
+		p.file = filepath.Join(p.Dir, filepath.Base(p.file))
+	}
+	type args struct {
+		path       string // path to package
+		pkgDirJoin string // override package directory
+		depDirJoin string // override dependency directory
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{"work with workspace protocol", args{"wsFoo.json", "", ""}, true, false},
+		{"don't work if path mismatch", args{"devBaz.json", "", ""}, false, true},                                               // "devBaz": "local:../devBaz" -> not rewriting dir should not work
+		{"don't work if pckg is outside of the workspace", args{"devBaz.json", "../devBaz", ""}, false, true},                   // "devBaz": "local:../devBaz" -> package outside the workspace should not work
+		{"work with file protocol inside the workspace", args{"devBaz.json", "packages/devBaz", "packages/fooer"}, true, false}, // "devBaz": "local:../devBaz" -> both resolving in the workspace should work
+		{"Don't work if version mismatch", args{"devFoo.json", "", ""}, false, true},
+		// @TODO add missing test cases
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testPkg, err := Read("./testdata/pkgStrings.json")
+			assert.NilError(t, err)
+			// override testPkgDir to force dependency dir
+			if tt.args.depDirJoin != "" {
+				overridePkgDir(testPkg, tt.args.depDirJoin)
+			}
+			pkg, err := Read(filepath.Join("./testdata", tt.args.path))
+			assert.NilError(t, err, "error reading package %s", tt.args.path)
+			// override pkgDir
+			if tt.args.pkgDirJoin != "" {
+				overridePkgDir(pkg, tt.args.pkgDirJoin)
+			}
+
+			dep, ok := testPkg.GetDepencyInfoFor(pkg.Name)
+			assert.Assert(t, ok, "can't find depency info for %s", pkg.Name)
+			satisfy, err := pkg.SatisfyWorskpaceDep(dep, "./testdata")
+			assert.Assert(t, satisfy == tt.want, "satisfyWorskpaceDep(%s) = %v, want %v, err %v", dep, tt.want, satisfy, err)
+			if tt.wantErr && err == nil {
+				t.Fatalf("Should return an error")
+			} else if !tt.wantErr && err != nil {
+				t.Fatalf("Should not return an error and get %s", err)
+			}
+		})
+	}
+}
--- package-json_test.go
+++ package-json_test.go
@@ -1,51 +1,199 @@
 package packageJson
 
 import (
+	"path/filepath"
+	"sort"
 	"testing"
+
+	"gotest.tools/v3/assert"
 )
 
-func TestReadPackageJson(t *testing.T) {
+func TestRead(t *testing.T) {
 
-	equalInterface := func(a map[string]interface{}, b map[string]string) bool {
-		for k, v := range a {
-			if b[k] != v.(string) {
-				return false
-			}
-		}
-		return true
-	}
+	pkgUnknwon, err := Read("./testdata/missing.json")
+	assert.ErrorContains(t, err, "no such file or directory")
+	assert.Assert(t, pkgUnknwon == nil)
+	pkgInvalid, err := Read("./testdata/invalid.json")
+	assert.ErrorIs(t, err, ErrInvalidJson)
+	assert.Assert(t, pkgInvalid == nil)
+	pkgStrings, err := Read("./testdata/pkgStrings.json")
+	assert.NilError(t, err)
+	assert.Assert(t, pkgStrings != nil)
+	pkgObjects, err := Read("./testdata/pkgObjects.json")
+	assert.NilError(t, err)
+	assert.Assert(t, pkgObjects != nil)
+	workspaces, err := Read("./testdata/workspaces.json")
+	assert.NilError(t, err)
+	assert.Assert(t, workspaces != nil)
+	mixedFundings, err := Read("./testdata/fundingAsMixedArray.json")
+	assert.NilError(t, err)
+	assert.Assert(t, mixedFundings != nil)
 
 	type args struct {
-		path string
+		pkg *PackageJSON
 	}
 	tests := []struct {
-		name    string
-		args    args
-		assert  func(*PackageJSON) bool
-		wantErr bool
+		name   string
+		args   args
+		assert func(*testing.T, *PackageJSON)
 	}{
-		{"read author as string", args{path: "./testdata/authorAsString.json"}, func(p *PackageJSON) bool {
-			return p.Author == "Barney Rubble <b@rubble.com> (http://barnyrubble.tumblr.com/)"
-		}, false},
-		{"read author as Objet", args{path: "./testdata/authorAsObject.json"}, func(p *PackageJSON) bool {
-			return equalInterface(p.Author.(map[string]interface{}), map[string]string{
-				"name":  "Barney Rubble",
-				"email": "b@rubble.com",
-				"url":   "http://barnyrubble.tumblr.com/",
-			})
-		}, false},
+		{
+			"read corectly as string",
+			args{pkg: pkgStrings},
+			func(t *testing.T, pkg *PackageJSON) {
+				assert.Assert(t, pkg.Name == "fooer")
+				assert.Assert(t, pkg.Version == "1.2.3")
+				assert.Assert(t, pkg.Description == "A packaged fooer for fooing foos")
+				assert.Assert(t, pkg.Main == "fooer.js")
+				assert.Assert(t, pkg.Man == "./man/doc.1")
+				assert.Assert(t, pkg.Bin == "path/to/bin")
+				assert.Assert(t, pkg.Author == "Barney Rubble <b@rubble.com> (http://barnyrubble.tumblr.com/)")
+				assert.Assert(t, pkg.Funding == "http://example.com/donate")
+			},
+		},
+		{
+			"read correctly as Objet",
+			args{pkg: pkgObjects},
+			func(t *testing.T, p *PackageJSON) {
+				assert.DeepEqual(t, p.Author, map[string]any{
+					"name":  string("Barney Rubble"),
+					"email": string("b@rubble.com"),
+					"url":   string("http://barnyrubble.tumblr.com/"),
+				})
+				assert.DeepEqual(t, p.Bin, map[string]any{
+					"my-program":       string("./path/to/program"),
+					"my-other-program": string("./path/to/other/program"),
+				})
+				assert.DeepEqual(t, p.Funding, map[string]any{
+					"type": string("individual"),
+					"url":  string("http://example.com/donate"),
+				})
+				assert.DeepEqual(t, p.Man, []any{
+					string("./man/foo.1"),
+					string("./man/bar.1"),
+				})
+			},
+		},
+		{
+			"read correctly workspaces",
+			args{pkg: workspaces},
+			func(t *testing.T, p *PackageJSON) {
+				assert.DeepEqual(t, p.Workspaces, []string{
+					"apps/*",
+					"packages/**",
+					"!**/tests/**",
+				})
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) { tt.assert(t, tt.args.pkg) })
+	}
+}
+
+func TestPackageJSON_GetMergedDependencies(t *testing.T) {
+	pkg, err := Read("./testdata/pkgStrings.json")
+	assert.NilError(t, err)
+	assert.DeepEqual(t, pkg.GetMergedDependencies(), map[string]string{
+		"optFoo": "~7.8.9",
+		"devFoo": "<=4.5.6",
+		"devBar": "npm:0.0.1",
+		"devBaz": "file:../devBaz",
+		"foo":    "^1.2.3",
+		"wsFoo":  "workspace:*",
+	})
+}
+
+func TestPackageJSON_GetAvailableTasks(t *testing.T) {
+	pkg, err := Read("./testdata/pkgStrings.json")
+	assert.NilError(t, err)
+	availables := pkg.GetAvailableTasks()
+	sort.Strings(availables)
+	assert.DeepEqual(t, availables, []string{"start", "test"})
+	pkg, err = Read("./testdata/fundingAsMixedArray.json")
+	assert.NilError(t, err)
+	assert.Assert(t, len(pkg.GetAvailableTasks()) == 0)
+}
+
+func TestPackageJSON_HasTask(t *testing.T) {
+	pkg, err := Read("./testdata/pkgStrings.json")
+	assert.NilError(t, err)
+	assert.Assert(t, pkg.HasTask("start"))
+	pkg, err = Read("./testdata/fundingAsMixedArray.json")
+	assert.NilError(t, err)
+	assert.Assert(t, !pkg.HasTask("start"))
+
+}
+
+func TestPackageJSON_FilterWorkspaceDirs(t *testing.T) {
+	rootDir, _ := filepath.Abs("./testdata/")
+	pkg, err := Read("./testdata/workspaces.json")
+	testDirs := []string{
+		"apps/test",
+		"apps/test/resources",
+		"packages/foo",
+		"packages/bar",
+		"packages/bar/baz",
+		"apps/tests/doc",
+		"packages/bar/tests",
+	}
+	assert.NilError(t, err)
+	assert.DeepEqual(t, pkg.FilterWorkspaceDirs(testDirs, false), []string{
+		"apps/test",
+		"packages/foo",
+		"packages/bar",
+		"packages/bar/baz",
+	})
+	assert.DeepEqual(t, pkg.FilterWorkspaceDirs(testDirs, true), []string{
+		filepath.Join(rootDir, "apps/test"),
+		filepath.Join(rootDir, "packages/foo"),
+		filepath.Join(rootDir, "packages/bar"),
+		filepath.Join(rootDir, "packages/bar/baz"),
+	})
+}
+
+func TestPackageJSON_GetDepencyInfoFor(t *testing.T) {
+	pkg, _ := Read("./testdata/pkgStrings.json")
+	type want struct {
+		Key          string
+		Name         string
+		VersionRange string
+		Protocol     string
+	}
+	compareDepInfo := func(t *testing.T, i PackageJsonDepInfo, w want) {
+		if w.Key == "" {
+			assert.DeepEqual(t, i, PackageJsonDepInfo{})
+			return
+		}
+		assert.Assert(t, i.FromName == "fooer", "expected fooer, got %s", i.FromName)
+		assert.Assert(t, i.FromVersion == "1.2.3", "expected 1.2.3, got %s", i.FromVersion)
+		assert.Assert(t, i.FromFile == pkg.file, "expected %s, got %s", pkg.file, i.FromFile)
+		assert.Assert(t, i.Name == w.Name, "expected %s, got %s", w.Name, i.Name)
+		assert.Assert(t, i.VersionRange == w.VersionRange, "expected %s, got %s", w.VersionRange, i.VersionRange)
+		assert.Assert(t, i.Key == w.Key, "expected %s, got %s", w.Key, i.Key)
+		assert.Assert(t, i.Protocol == w.Protocol, "expected %s, got %s", w.Protocol, i.Protocol)
+	}
+	tests := []struct {
+		name   string
+		want   want
+		wantOk bool
+	}{
+		{"retrieving dependency info", want{"dependencies", "foo", "^1.2.3", ""}, true},
+		{"retrieving devDependency info", want{"devDependencies", "devFoo", "<=4.5.6", ""}, true},
+		{"retrieving devDependency info", want{"devDependencies", "devBar", "0.0.1", "npm"}, true},
+		{"retrieving devDependency info", want{"devDependencies", "devBaz", "../devBaz", "file"}, true},
+		{"retrieving optionalDependency info", want{"optionalDependencies", "optFoo", "~7.8.9", ""}, true},
+		{"retrieving peerDependency info", want{"peerDependencies", "peerFoo", "10.11.x", ""}, true},
+		{"retrieving dependency with protocol", want{"dependencies", "wsFoo", "*", "workspace"}, true},
+		{"retrieving unknown dependency", want{"", "Unknwon", "", ""}, false},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Read(tt.args.path)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("ReadPackageJson() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !tt.assert(got) {
-				t.Errorf("ReadPackageJson():  = %v", got)
-			}
+			got, got1 := pkg.GetDepencyInfoFor(tt.want.Name)
+			compareDepInfo(t, got, tt.want)
+			assert.Equal(t, got1, tt.wantOk)
 		})
 	}
 }
